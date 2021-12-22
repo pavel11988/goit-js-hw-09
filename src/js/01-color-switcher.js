96c7e02a7f7d388aae7d8aae6effde8fed17b26d
pavel11988 <kossoua88@gmail.com>
2021-12-22 15:00:37 +0200

    task-1_completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,49 @@
 
+const button_start = document.querySelector("button[data-start]")
+const button_stop  = document.querySelector("button[data-stop]")
+
+// button_start.addEventListener('click',
+
+const changeColor = {
+    timerId: null,
+    isActive: false,
+
+
+    start(){
+        if (this.isActive)  {
+            button_stop.classList.add("disabled");
+            return;
+        }
+
+        this.isActive = true;
+        button_start.classList.add("disabled");
+        button_stop.classList.remove("disabled");
+
+        timerId = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+        
+    },
+
+    stop(){
+        if (this.isActive)  {
+            button_stop.classList.add("disabled");
+            button_start.classList.remove("disabled");
+        }
+        this.isActive = false;
+        clearInterval(timerId);
+    }
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+button_start.addEventListener('click', () => {
+    changeColor.start();
+})
+
+button_stop.addEventListener('click', () => {
+    changeColor.stop();
+})
