bb155d0d24305b9f19d2557bfe654aba845624b8
pavel11988 <kossoua88@gmail.com>
2021-12-27 01:55:44 +0200

    add-second-r

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,26 +10,37 @@ function onButtonCreate(event) {
   let delay = +event.currentTarget.elements.delay.value;
   let step = +event.currentTarget.elements.step.value;
   const amount = +event.currentTarget.elements.amount.value;
-  let array = [];
 
-
-  if (delay < 0 || step < 0 || amount < 0) {
-    Notiflix.Notify.warning('Error! Values must be >= 0')
-    return;
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position,delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      })
+      delay += step;
+    }   
   }
 
-  for (let i = 0; i < amount; i += 1) {
-    array.push(delay + i * step);
-  }
+  //Через цикл и массив:
+  // let array = [];
+  // if (delay < 0 || step < 0 || amount < 0) {
+  //   Notiflix.Notify.warning('Error! Values must be >= 0')
+  //   return;
+  // }
 
-  const promises = array.map((value, index) => {
-    createPromise(index + 1, value)
-      .then(({i, delay}) => Notiflix.Notify.success(`Fulfilled promise ${index + 1} in ${value}ms`))
-      .catch(({i, delay}) => Notiflix.Notify.failure(`Rejected promise ${index + 1} in ${value}ms`))
-  })
-  
-} 
+  // for (let i = 0; i < amount; i += 1) {
+  //   array.push(delay + i * step);
+  // }
 
+  // const promises = array.map((value, index) => {
+  //   createPromise(index + 1, value)
+  //     .then(({i, delay}) => Notiflix.Notify.success(`Fulfilled promise ${index + 1} in ${value}ms`))
+  //     .catch(({i, delay}) => Notiflix.Notify.failure(`Rejected promise ${index + 1} in ${value}ms`))
+  // })
+// }
+ 
 
 function createPromise(position, delay) {
 
