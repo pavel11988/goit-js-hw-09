a186930a46d8217e7ff2c977da64fd17a5dcb4b6
pavel11988 <kossoua88@gmail.com>
2021-12-26 18:31:11 +0200

    task-3 completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+const form = document.querySelector('.form');
+let position = 0;
+let timerId = null;
+
+form.addEventListener('submit', onButtonCreate);
+
+function onButtonCreate(event) {
+  event.preventDefault();
+  let delay = +event.currentTarget.elements.delay.value;
+  const step = +event.currentTarget.elements.step.value;
+  const amount = +event.currentTarget.elements.amount.value;
+
+
+  timerId = setInterval(() => {
+
+    position += 1;  
+    if (position == amount+1) {clearInterval(timerId); return}
+
+    setTimeout(() => delay += step) 
+
+    createPromise(position, delay)
+      .then(({position, delay}) => console.log(`✅ Fulfilled promise ${position} in ${delay}ms`))
+      .catch(({position, delay}) => console.log(`❌ Rejected promise ${position} in ${delay}ms`))
+  },step)
+} 
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  const promise = new Promise((reslove , reject) => {
+   
+    setInterval(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if(shouldResolve) reslove({position, delay});
+      else reject({position, delay});
+
+      },delay);
+  })
+
+  return promise;
+}  
+
